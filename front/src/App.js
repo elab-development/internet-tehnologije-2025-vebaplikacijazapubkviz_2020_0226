3f69a5550ae624bc44e0d8dc596f474951e03b72
stojadinoviclaza-afk <stojadinoviclaza@gmail.com>
2026-01-24 20:33:02 +0100

    Postavljena struktura aplikacije

---
+++ front/src/App.js
@@ -0,0 +1,24 @@
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from "react-router-dom";
+
+
+function App() {
+  return (
+    <Router>
+      <div className="min-h-screen bg-gray-50">
+        <main className="max-w-7xl mx-auto px-4 py-10">
+          <Routes>
+            <Route path="*" element={<Navigate to="/" />} />
+          </Routes>
+        </main>
+      </div>
+    </Router>
+  );
+}
+
+export default App;

5e8f0b82e24d989f33e67cafafceafad5829a89a
stojadinoviclaza-afk <stojadinoviclaza@gmail.com>
2026-02-22 19:26:15 +0100

    Dodata swagger dokumentacija

--- back/app/Providers/AppServiceProvider.php
+++ back/app/Providers/AppServiceProvider.php
@@ -3,7 +3,11 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
-
+use Illuminate\Support\Facades\URL;
+use Illuminate\Support\Facades\Gate;
+use Dedoc\Scramble\Scramble;
+use Dedoc\Scramble\Support\Generator\OpenApi;
+use Dedoc\Scramble\Support\Generator\SecurityScheme;
 class AppServiceProvider extends ServiceProvider
 {
     /**
@@ -19,6 +23,24 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+         if (app()->environment('production')) {
+        URL::forceScheme('https');
+    }
+
+        Gate::define('viewApiDocs', function ($user = null) {
+            if (app()->environment('local')) {
+                return true;
+            }
+
+           return $user?->role === 'moderator';
+        });
+
+        
+
+        Scramble::afterOpenApiGenerated(function (OpenApi $openApi) {
+        $openApi->secure(
+            SecurityScheme::http('bearer')
+        );
+    });
     }
 }
